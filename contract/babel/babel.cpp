f8fcdbe2da68afae4e8b849abdb872cc25aba3cb
ganjingcun <343747757@qq.com>
2018-06-09 21:51:18 +0800

    add vote api

--- contract/babel/babel.cpp
+++ contract/babel/babel.cpp
@@ -61,23 +61,47 @@ class babel : public eosio::contract
     }
 
     // @abi action
-    void like(account_name author, const uint32_t id, const uint64_t status)
+    void like(account_name author, account_name user, const uint64_t id)
     {
         video_index videos(_self, author);
         auto v_itr = videos.find(id);
+        eosio_assert(v_itr != videos.end(), "videos not found ");
 
-        user_index users(_self, author);
-        auto u_itr = users.find(author);
+        user_index author_table(_self, author);
+        auto a_itr = author_table.find(author);
+        eosio_assert(a_itr != author_table.end(), "unknown author");
 
-        eosio_assert(u_itr != users.end(), "unknown account");
+        user_index users_table(_self, user);
+        auto u_itr = users_table.find(user);
+        eosio_assert(u_itr != users_table.end(), "unknown user");
 
-        // video vote + 1
+        // video balance 、 vote + 1
+        videos.modify(v_itr, 0, [&](auto &_video) {
+            _video.ups++;
+            _video.balance += asset(10000, S(4, DJB));
+        });
+
+        // author ++ up
+        author_table.modify(a_itr, 0, [&](auto &acnt) {
+            acnt.ups++;
+        });
 
         // voter balance +
+        users_table.modify(u_itr, 0, [&](auto &acnt) {
+            acnt.balance += asset(10000, S(4, DJB));
+        });
 
-        //
+        log_index logs(_self, user);
 
-        eosio::print("like#", author, status, " created");
+        logs.emplace(_self, [&](auto &new_log) {
+            new_log.id = logs.available_primary_key();
+            new_log.owner = author;
+            new_log.type = 1;
+            new_log.description = "Vote";
+            new_log.quantity = asset(10000, S(4, DJB));
+        });
+
+        eosio::print("like#", author, " success");
     }
 
     // @abi action
@@ -97,37 +121,95 @@ class babel : public eosio::contract
 
         user_index users_inviter(_self, inviter);
         auto inviter_itr = users_inviter.find(inviter);
+
+        eosio::print("booking#", bar, user, quantity, inviter);
+
         if (inviter_itr != users_inviter.end())
         {
-            // 消费 100% 
+            eosio::print("booking has inviter#");
+
+            // 消费 100%
             users.modify(users_itr, 0, [&](auto &acnt) {
                 eosio_assert(acnt.balance >= quantity, "insufficient balance");
-                acnt.balance -= quantity ;
+                acnt.balance -= quantity;
             });
 
-             // 酒吧 80%
+            log_index logs(_self, user);
+
+            logs.emplace(_self, [&](auto &new_log) {
+                new_log.id = logs.available_primary_key();
+                new_log.owner = user;
+                new_log.type = -1;
+                new_log.description = "订座";
+                new_log.quantity = quantity;
+            });
+
+            // 酒吧 80%
             users_bar.modify(bar_itr, 0, [&](auto &acnt) {
-                acnt.balance += quantity * 0.8;
+                acnt.balance += asset(quantity.amount * 0.8, S(4, DJB));
             });
 
-            // 佣金 20% 
+            log_index bar_logs(_self, bar);
+
+            bar_logs.emplace(_self, [&](auto &new_log) {
+                new_log.id = bar_logs.available_primary_key();
+                new_log.owner = bar;
+                new_log.type = 1;
+                new_log.description = "订座";
+                new_log.quantity = asset(quantity.amount * 0.8, S(4, DJB));
+                ;
+            });
+
+            // 佣金 20%
             users_inviter.modify(inviter_itr, 0, [&](auto &acnt) {
-                acnt.balance += quantity * 0.2;
+                acnt.balance += asset(quantity.amount * 0.2, S(4, DJB));
+            });
+
+            log_index inviter_logs(_self, inviter);
+
+            inviter_logs.emplace(_self, [&](auto &new_log) {
+                new_log.id = inviter_logs.available_primary_key();
+                new_log.owner = inviter;
+                new_log.type = 1;
+                new_log.description = "推广分佣";
+                new_log.quantity = asset(quantity.amount * 0.2, S(4, DJB));
             });
         }
         else
         {
-             // 消费 100% 
+
+            eosio::print("booking no inviter#", bar, user, quantity, inviter);
+            // 消费 100%
             users.modify(users_itr, 0, [&](auto &acnt) {
                 eosio_assert(acnt.balance >= quantity, "insufficient balance");
-                acnt.balance -= quantity ;
+                acnt.balance -= quantity;
             });
 
-             // 酒吧 100%
+            log_index logs(_self, user);
+
+            logs.emplace(_self, [&](auto &new_log) {
+                new_log.id = logs.available_primary_key();
+                new_log.owner = user;
+                new_log.type = -1;
+                new_log.description = "订座";
+                new_log.quantity = quantity;
+            });
+
+            // 酒吧 100%
             users_bar.modify(bar_itr, 0, [&](auto &acnt) {
-                acnt.balance += quantity ;
+                acnt.balance += quantity;
             });
 
+            log_index bar_logs(_self, bar);
+
+            bar_logs.emplace(_self, [&](auto &new_log) {
+                new_log.id = bar_logs.available_primary_key();
+                new_log.owner = bar;
+                new_log.type = 1;
+                new_log.description = "订座";
+                new_log.quantity = quantity;
+                ;
+            });
         }
     }
 
@@ -137,7 +219,7 @@ class babel : public eosio::contract
 
     // 3. 用户每天可以点赞10次，点赞获得10 DJB
 
-    // 分享按钮 生成视频分析链接 链接中带入 推广人id、酒吧id
+    // 分享按钮 生成视频分析链接 链接中带入 推广人xx、酒吧xx
 
     // 另一个用户 点击视频分享 获取 推广人id、酒吧id。
 
@@ -187,16 +269,16 @@ class babel : public eosio::contract
 
         uint64_t primary_key() const { return owner; }
 
-        asset balance = asset(100, S(4, DJB));
-        uint32_t flag = 0;    //标记 0 普通群众，1 酒吧， 2-8 DJ，艺人等角色
-        uint32_t age = 0;     //年纪
-        uint32_t address = 0; //地址
-        uint32_t fans = 0;    //粉丝
-        uint32_t follow = 0;  //关注
-        uint32_t ups = 0;     //赞
-        uint32_t coin = 0;    //币值
-        uint32_t card = 0;    //卡券
-        uint32_t energy = 0;  //卡券
+        asset balance = asset(5000000, S(4, DJB));
+        uint64_t flag = 0;    //标记 0 普通群众，1 酒吧， 2-8 DJ，艺人等角色
+        uint64_t age = 0;     //年纪
+        uint64_t address = 0; //地址
+        uint64_t fans = 0;    //粉丝
+        uint64_t follow = 0;  //关注
+        uint64_t ups = 0;     //赞
+        uint64_t coin = 0;    //币值
+        uint64_t card = 0;    //卡券
+        uint64_t energy = 0;  //卡券
 
         std::string name;   //昵称
         std::string avatar; //头像
@@ -204,11 +286,25 @@ class babel : public eosio::contract
         EOSLIB_SERIALIZE(user, (id)(owner)(balance)(flag)(age)(address)(fans)(follow)(ups)(coin)(card)(energy)(name)(avatar))
     };
 
+    // @abi table log i64
+    struct log
+    {
+        uint64_t id;
+        uint64_t type; //增、减
+        account_name owner;
+        std::string description;
+        asset quantity;
+
+        uint64_t primary_key() const { return id; }
+        EOSLIB_SERIALIZE(log, (id)(type)(owner)(description)(quantity))
+    };
+
     // @abi table video i64
     struct video
     {
         uint64_t id;
         account_name owner;
+        asset balance = asset(0, S(4, DJB));
         std::string description;
         std::string title;
         std::string address;
@@ -217,12 +313,14 @@ class babel : public eosio::contract
         uint64_t share;
 
         uint64_t primary_key() const { return id; }
-        EOSLIB_SERIALIZE(video, (id)(owner)(description)(title)(address)(ups)(comment)(share))
+        EOSLIB_SERIALIZE(video, (id)(owner)(balance)(description)(title)(address)(ups)(comment)(share))
     };
 
     typedef eosio::multi_index<N(user), user> user_index;
 
     typedef eosio::multi_index<N(video), video> video_index;
+
+    typedef eosio::multi_index<N(log), log> log_index;
 };
 
 EOSIO_ABI(babel, (hi)(signup)(post)(like)(add)(sub)(booking))
